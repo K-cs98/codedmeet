5572a2dd65973d1e704c2771135d8470dee1bf2e
knorrnation1998-svg <knorrnation1998@gmail.com>
2026-08-27 17:10:12 +0100

    Bypass typecheck on build

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Allows production builds to successfully complete even if project has type errors
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignore ESLint errors during builds as well
     ignoreDuringBuilds: true,
   },
 };
